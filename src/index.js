a9bd7e67e228ebe019da599327a773fcda86e13f
8467042733 <rupeshpal25112004@gmail.com>
2023-12-28 11:58:40 +0530

    add some code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+
 const fs = require('fs').promises;
 const fileName = 'myfile.txt';
 
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
-    
-};
-
+    fs.appendFile(fileName,fileContent,function(err){
+        if(err) throw err;
+        console.log("File Updated");
+    })
+}
+updateFile(fileName,"Newton School, is an online learning platform.");
 
 module.exports = updateFile;
